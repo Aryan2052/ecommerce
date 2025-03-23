2bb4f80ba5a4d45f35d6113205542719411a9816
Aryan2052 <aryandangat2005@gmail.com>
2025-03-23 22:00:02 +0530

    frontend working

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Cart from './pages/Cart';
+import Order from './pages/Order';
+import './styles/App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<Order />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
